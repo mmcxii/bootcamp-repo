d25991062a24f113465dc95fdf42e4db29ca11b3
Nich Secord <nicholassecord@gmail.com>
2019-06-04 20:19:46 -0700

    made data case insensitive

--- block-3/3.2/20-HardLoop/script.js
+++ block-3/3.2/20-HardLoop/script.js
@@ -3,9 +3,9 @@ const myFarm = ['chickens', 'pigs', 'cows', 'horses', 'ostriches'];
 myFarm.forEach((animal) => {
     console.log(animal);
 
-    if (animal[0] === 'c') {
+    if (animal[0].toLowerCase() === 'c') {
         alert(`${animal} starts with C!`);
-    } else if (animal[0] === 'o') {
+    } else if (animal[0].toLowerCase() === 'o') {
         alert(`${animal} starts with O!`);
     }
 });

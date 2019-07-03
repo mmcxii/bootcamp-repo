ee524cd5c8585a9572e26af5d3d42318dc731526
Nich Secord <nicholassecord@gmail.com>
2019-07-02 21:32:17 -0700

    finished 7.2

--- block-7/7.2/11-countdownbutton/Unsolved/logicOption2.js
+++ block-7/7.2/11-countdownbutton/Unsolved/logicOption2.js
@@ -1,4 +1,17 @@
 // Initialize Firebase (YOUR OWN APP)
+const firebaseConfig = {
+    apiKey: 'AIzaSyCeoZ8F3J9Jrtr2NpEyPAUrqW_et0fEeiY',
+    authDomain: 'in-class-test-746ef.firebaseapp.com',
+    databaseURL: 'https://in-class-test-746ef.firebaseio.com',
+    projectId: 'in-class-test-746ef',
+    storageBucket: '',
+    messagingSenderId: '949053777825',
+    appId: '1:949053777825:web:79a2db51506fda6f',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
 
 // Set Initial Counter
 var initialValue = 100;
@@ -8,56 +21,65 @@ var clickCounter = initialValue;
 // --------------------------------------------------------------
 
 // At the initial load, get a snapshot of the current data.
+db.ref().on(
+    'value',
+    (res) => {
+        // Print the initial data to the console.
+        console.log(res.val());
 
+        // Change the html to reflect the initial value.
+        const count = res.val().clickCount;
 
-// Print the initial data to the console.
+        $('#click-value').text(count);
 
+        // Change the clickCounter to match the data in the database
+        clickCounter = count;
 
-// Change the html to reflect the initial value.
+        // Log the value of the clickCounter
+        console.log(clickCounter);
 
-
-// Change the clickCounter to match the data in the database
-
-
-// Log the value of the clickCounter
-
-
-// Change the HTML Value
-
-
-// If any errors are experienced, log them to console.
+        // Change the HTML Value
+        $('#click-value').text(clickCounter);
+    },
+    // If any errors are experienced, log them to console.
+    (err) => console.error(err)
+);
 
 // --------------------------------------------------------------
 
 // Whenever a user clicks the click button
-$("#click-button").on("click", function() {
+$('#click-button').on('click', function() {
+    // Reduce the clickCounter by 1
+    clickCounter--;
 
-  // Reduce the clickCounter by 1
+    if (clickCounter === 0) {
+        // Alert User and reset the counter
+        alert(`Congrats on clicking a button ${initialValue} times. I guess.`);
 
+        clickCounter = initialValue;
+    }
 
-  // Alert User and reset the counter
-
-
-  // Save new value to Firebase
-
-
-  // Log the value of clickCounter
-
-
+    // Save new value to Firebase
+    db.ref().set({
+        clickCount: clickCounter,
+    });
+    // Log the value of clickCounter
+    console.log(clickCounter);
 });
 
 // Whenever a user clicks the restart button
-$("#restart-button").on("click", function() {
+$('#restart-button').on('click', function() {
+    // Set the clickCounter back to initialValue
+    clickCounter = initialValue;
 
-  // Set the clickCounter back to initialValue
+    // Save new value to Firebase
+    db.ref().set({
+        clickCount: clickCounter,
+    });
 
+    // Log the value of clickCounter
+    console.log(clickCounter);
 
-  // Save new value to Firebase
-
-
-  // Log the value of clickCounter
-
-
-  // Change the HTML Values
-
+    // Change the HTML Values
+    $('#click-value').text(clickCounter);
 });
--- block-7/7.2/14-codersbay/Unsolved/logicOption2.js
+++ block-7/7.2/14-codersbay/Unsolved/logicOption2.js
@@ -1,14 +1,25 @@
 // Initialize Firebase
 // Make sure to match the configuration to the script version number in the HTML
 // (Ex. 3.0 != 3.7.0)
+var firebaseConfig = {
+    apiKey: 'AIzaSyCeoZ8F3J9Jrtr2NpEyPAUrqW_et0fEeiY',
+    authDomain: 'in-class-test-746ef.firebaseapp.com',
+    databaseURL: 'https://in-class-test-746ef.firebaseio.com',
+    projectId: 'in-class-test-746ef',
+    storageBucket: '',
+    messagingSenderId: '949053777825',
+    appId: '1:949053777825:web:79a2db51506fda6f',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 // Assign the reference to the database to a variable named 'database'
 // var database = ...
-
+const db = firebase.database();
 
 // Initial Values
 var initialBid = 0;
-var initialBidder = "No one :-(";
+var initialBidder = 'No one :-(';
 var highPrice = initialBid;
 var highBidder = initialBidder;
 
@@ -17,59 +28,84 @@ var highBidder = initialBidder;
 //  At the page load and subsequent value changes, get a snapshot of the stored data.
 // This function allows you to update your page in real-time when the firebase database changes.
 
+db.ref().on(
+    'value',
+    (res) => {
+        const data = res.val().codersBay;
+        console.log(data);
 
-// If Firebase has a highPrice and highBidder stored (first case)
+        // If Firebase has a highPrice and highBidder stored (first case)
+        if (data.highPrice !== undefined && data.highBidder !== undefined) {
+            // Set the variables for highBidder/highPrice equal to the stored values in firebase.
+            highPrice = data.highPrice;
+            highBidder = data.highBidder;
 
+            // Change the HTML to reflect the stored values
+            $('#highest-bidder').text(highBidder);
+            $('#highest-price').text(highPrice);
 
-// Set the variables for highBidder/highPrice equal to the stored values in firebase.
-// highPrice = ...
-// highBidder = ...
+            // Print the data to the console.
+            console.log(highBidder, highPrice);
 
+            // Else Firebase doesn't have a highPrice/highBidder, so use the initial local values.
+        } else {
+            // Change the HTML to reflect the initial values
+            $('#highest-bidder').text(highBidder);
+            $('#highest-price').text(highPrice);
 
-// Change the HTML to reflect the stored values
-
-
-// Print the data to the console.
-
-
-// Else Firebase doesn't have a highPrice/highBidder, so use the initial local values.
-
-
-// Change the HTML to reflect the initial values
-
-
-// Print the data to the console.
-
-
-
+            // Print the data to the console.
+        }
+    },
+    (err) => console.error(err)
+);
 
 // --------------------------------------------------------------
 
 // Whenever a user clicks the submit-bid button
+$('#submit-bid').on('click', (e) => {
+    // prevent form from submitting with event.preventDefault() or returning false
+    e.preventDefault();
 
-// prevent form from submitting with event.preventDefault() or returning false
+    // Get the input values
+    const bid = parseInt(
+        $('#bidder-price')
+            .val()
+            .trim()
+    );
+    const name = $('#bidder-name')
+        .val()
+        .trim();
 
-// Get the input values
+    // Log the Bidder and Price (Even if not the highest)
+    console.log(bid, name);
 
+    // If Then statements to compare against previous high bidder
 
-// Log the Bidder and Price (Even if not the highest)
+    if (bid === 1) highPrice = 0;
 
+    if (bid > highPrice) {
+        // Alert that they are High Bidder
+        alert('You are the new highest bidder!');
 
-// If Then statements to compare against previous high bidder
+        // Save the new price in Firebase
+        db.ref('codersBay').set({
+            highBidder: name,
+            highPrice: bid,
+        });
 
+        // Log the new High Price
+        console.log(bid);
 
-// Alert that they are High Bidder
+        // Store the new high price and bidder name as a local variable (could have also used the firebase variable)
+        highPrice = bid;
+        highBidder = name;
 
+        // Change the HTML to reflect the new high price and bidder
+        $('#highest-bidder').text(highBidder);
+        $('#highest-price').text(highPrice);
 
-// Save the new price in Firebase
-
-
-// Log the new High Price
-
-
-// Store the new high price and bidder name as a local variable (could have also used the firebase variable)
-
-
-// Change the HTML to reflect the new high price and bidder
-
-// Else tell user their bid was too low via alert
+        // Else tell user their bid was too low via alert
+    } else {
+        alert(`Your bid of ${bid} was not more than the current highest bid of ${highPrice}.`);
+    }
+});
